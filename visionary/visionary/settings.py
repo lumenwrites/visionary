4c70db54a8b3bf7f79de9b47a06fa17d4c059fff
raymestalez <raymestalez@gmail.com>
2014-07-06 14:54:58 +0000

    Initial Heroku Test setup

--- visionary/visionary/settings.py
+++ visionary/visionary/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'forum',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -109,3 +110,28 @@ except ImportError:
         from local_settings import *
     except:
         pass
+
+
+# Heroku Settings:
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
--- visionary/visionary/wsgi.py
+++ visionary/visionary/wsgi.py
@@ -10,5 +10,12 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visionary.settings")
 
+# Default
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+# Heroku
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
